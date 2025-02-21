fb33e75046cd8c6fa96ff5f993434b9712017e84
ZoltanSzarvas0209 <szarvas.zoltan@hotmail.com>
2025-02-21 13:42:41 +0000

    added views and urls to all portfolio templates

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.portfolio, name='portfolio')
+    path('', views.portfolio, name='portfolio'),
+    path('about/', views.about, name='about'),
+    path('tech/', views.tech, name='tech'),
+    path('projects/', views.projects, name='projects'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -4,6 +4,17 @@ from .models import Project
 # Create your views here.
 
 def portfolio(request):
-    """ A view to show all services available """
-
+    """ A view to show the portfolio page """
     return render(request, 'portfolio/portfolio.html')
+
+def about(request):
+    """ A view to show the about page """
+    return render(request, 'portfolio/about.html')
+
+def tech(request):
+    """ A view to show the technologies page """
+    return render(request, 'portfolio/tech.html')
+
+def projects(request):
+    """ A view to show the projects page """
+    return render(request, 'portfolio/projects.html')
