eaad49c0e48f50ff1455e694b703afd16c74888f
ZoltanSzarvas0209 <szarvas.zoltan@hotmail.com>
2025-02-21 12:12:09 +0000

    added service template, view and urls

--- services/views.py
+++ services/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Service
 
 # Create your views here.
+
+def all_services(request):
+    """ A view to show all services available """
+
+    services = Service.objects.all()
+
+    context = {
+        'services': services,
+    }
+
+    return render(request, 'services/services.html', context)
