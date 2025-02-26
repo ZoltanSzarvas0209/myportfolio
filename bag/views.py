24dd3bea29f3acc8eb3b65a5cc187014bb67ff14
ZoltanSzarvas0209 <szarvas.zoltan@hotmail.com>
2025-02-26 19:53:10 +0000

    added javascript, views to adjust shopping cart

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.http import JsonResponse
+
 
 # Create your views here.
 
@@ -7,7 +9,7 @@ def bag(request):
 
     return render(request, 'bag/bag.html')
 
-# view is a duplicate from Bourique Ado project from CodeInstitute tutorial , which is revised to suit this project.
+# view is a duplicate from Bourique Ado project from CodeInstitute tutorial ,  was revised to suit this project.
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified service to the shopping bag """
 
@@ -22,3 +24,32 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the specified item from the shopping bag"""
+    try:
+        bag = request.session.get('bag', {})
+        if str(item_id) in bag:
+            del bag[str(item_id)]
+            request.session['bag'] = bag
+            return JsonResponse({'success': True})
+        else:
+            return JsonResponse({'error': 'Item not in bag'}, status=400)
+
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
