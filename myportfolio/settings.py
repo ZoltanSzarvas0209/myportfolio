05a9d380c8c02ac63a3318c116ecbd1074258fdc
ZoltanSzarvas0209 <szarvas.zoltan@hotmail.com>
2025-02-27 17:52:56 +0000

    setting up stripe

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -14,6 +14,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+if os.path.isfile('env.py'):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -172,3 +175,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Crispy Forms Settings
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# Stripe
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
